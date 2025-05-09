1ee9d8788247caac4e3361a1e5fb4c35cb3a19ab
Daryl Hornick-Becker <darylhornickbecker@gmail.com>
2025-05-08 20:06:15 -0400

    Done

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express"
+import playlist from "#db/playlist"
+const app = express()
+
+app.route('/').get((req, res)=>{
+    res.send("You've reached the Playlist API!")
+})
+
+app.route('/playlist').get((req, res)=>{
+    res.send(playlist)
+})
+
+app.route("/playlist/:index").get((req, res)=>{
+    const idx = Number(req.params.index)
+    const found = playlist[idx]
+    if (found){
+        res.send(found)
+    } else {
+        res.status(404).send("That song does not exist in the playlist.")
+    }
+})
+
+
+export default app
